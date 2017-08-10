1e0a3613e7c2a2e487fc1ab3cf10a2f161779ca9
emilio <emiliorizzo@gmail.com>
2017-08-10 05:08:47 -0300

    Add network graph (vue-d3-network)

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <template lang="pug">
-  .test
-    h1 {{connected}}
+  #app.main(@keyup.esc='setTool("pointer")')
+    //-> Network
+    d3-network(
+      v-if='connected'
+      :netNodes="nodes"
+      :netLinks="links"
+      :options="options"
+      class="net"
+      )
+    //- if not connected  
+    .center.loading(v-else)
+      img(class="logo" src="static/rsk-logo.png")
+      h1.center connecting to server  
+
 </template>
 <script>
+/* eslint-disable no-unused-vars */
+import { mapState, mapActions, mapGetters } from 'vuex'
+import defaultData from './data.js'
+import D3Network from 'vue-d3-network'
 export default {
-  name: 'app',
+  name: 'NetStats',
+  components: {
+    D3Network
+  },
+  data () {
+    let data = Object.assign({}, defaultData)
+    data.options.size = { w: 500, h: 500 }
+    data.options.offset = { x: 0, y: 0 }
+    return data
+  },
+  mounted () {
+    this.onResize()
+    window.addEventListener('resize', this.onResize)
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onResize)
+  },
   computed: {
-    connected () {
-      return this.$store.state.socketConnected
+    ...mapState({
+      state: state => state,
+      totals: state => state.totals
+    }),
+    ...mapGetters({
+      size: 'getSize',
+      connected: 'isConnected',
+      nodes: 'getNodesArr',
+      links: 'getLinksArr',
+      activeNodes: 'getActivesNodes'
+    })
+  },
+  methods: {
+    onResize () {
+      let size = { w: this.$el.clientWidth, h: this.$el.clientHeight }
+      this.setSize(size)
+      let options = this.options
+      options.size = size
+      this.changeOptions(options)
     }
   }
 }
 </script>
-
 <style lang="stylus">
-
 </style>
-
---
+++ src/data.js
@@ -0,0 +1,11 @@
+export default {
+  options: {
+    nodeSym: null,
+    force: 2500,
+    nodeSize: 30,
+    fontSize: 16,
+    linkWidth: 2,
+    nodeLabels: true,
+    strLinks: true
+  }
+}
