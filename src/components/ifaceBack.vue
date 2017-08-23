dd933bc38e5000583c5fc58e9440f50cd76fe32e
emilio <emiliorizzo@gmail.com>
2017-08-23 10:01:05 -0300

    Add block numbers animation over nodes

--- src/components/ifaceBack.vue
+++ src/components/ifaceBack.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-  .iface-back
+  .iface-back(:width='size.w + "px"' :height='size.h + "px"')
+    .node-block(v-for='node,id in nodes' :key='id' :style='nodeBlockStyle(node)')
+      transition(name="block-number" type="animation"  appear )
+        span.block-number( :key='node.stats.block.number') {{nodes[id].stats.block.number}}
     svg(:width='size.w' :height='size.h')
       //- selection lines
       g(id="selections" class="selection-lines" v-for='dialog,key in dialogs')
@@ -12,13 +15,17 @@ export default {
   props: ['size'],
   computed: {
     ...mapState({
-      dialogs: state => state.app.nodeDialogs
+      dialogs: state => state.app.nodeDialogs,
+      nodes: state => state.backendData.nodes
     })
   },
   methods: {
-    ...mapGetters(
-      ['getNode']
-    ),
+    ...mapGetters([
+      'getNode'
+    ]),
+    nodeBlockStyle (node) {
+      return 'left:' + node.x + 'px; top:' + node.y + 'px'
+    },
     quadCurve (dialog) {
       let node = this.getNode()(dialog.id)
       let t = 0.6 + (0.1 / this.size.w * (dialog.x - dialog.w))
@@ -36,3 +43,30 @@ export default {
   }
 }
 </script>
+<style lang="stylus">
+@import '../lib/styl/vars.styl'
+  .node-block
+    position: absolute
+    font-size: .8em
+  .block-number-enter-active
+    color: $color
+    position: absolute
+    top:0
+    left:0
+    animation-name: blocknumber-anim
+    animation-duration: 2s
+    animation-iteration-count: 1
+    opacity: 0
+  .block-number
+    opacity:0 
+  @keyframes blocknumber-anim
+    0%
+      opacity: 0
+    10%
+      opacity: 1  
+    100%     
+      opacity: 0
+      transform: translateY(-100px)
+
+</style>
+
