5fee76c06c7db4600cca79a6180f6a80a6d9866f
emilio <emiliorizzo@gmail.com>
2017-09-04 12:58:54 -0300

    Update app/entity add thresholds filters and fields

--- src/mixins/EntityCommon.js
+++ src/mixins/EntityCommon.js
@@ -1,6 +1,6 @@
 import { mapGetters } from 'vuex'
 export default {
-  props: ['entity', 'value'],
+  props: ['entity', 'value', 'fields'],
   data () {
     return {
       colorFunc: null
@@ -15,20 +15,42 @@ export default {
   },
   computed: {
     filteredValue () {
-      return this.filterValue()(this.entity, this.value)
+      return this.filterEntityValue()(this.entity, this.value)
     },
     color () {
       let cf = this.colorFunc
-      return (cf && this.value) ? cf(this.value) : ''
+      let value = this.thField || this.value
+      value = this.thFilter(value)
+      return (cf && value) ? cf(value) : ''
     },
     threshold () {
       return this.entity.threshold
+    },
+    thValue () {
+      let entity = this.entity
+      let fields = this.fields
+      if (entity && fields) {
+        let th = entity.thField
+        if (th) {
+          let thField = fields[th]
+          return thField
+        }
+      }
     }
   },
   methods: {
     ...mapGetters('app/entity', [
       'thresholdColors',
-      'filterValue'
-    ])
+      'filterEntityValue',
+      'applyFilter'
+    ]),
+    thFilter (value) {
+      let threshold = this.entity.thresholdObj
+      if (threshold) {
+        let filters = threshold.filters
+        value = this.applyFilter()(filters, value)
+      }
+      return value
+    }
   }
 }
