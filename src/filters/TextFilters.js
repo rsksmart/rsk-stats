71be5186afaf1fb88173968c41a3f538109d8e40
emilio <emiliorizzo@gmail.com>
2017-09-05 03:05:17 -0300

    Add filters

--- src/filters/TextFilters.js
+++ src/filters/TextFilters.js
@@ -3,3 +3,7 @@ import Vue from 'vue'
 export const nodeType = Vue.filter('node-type', (text) => {
   return text.replace(/\//g, ' ')
 })
+
+export const yesNo = Vue.filter('yes-no', (value) => {
+  return (value) ? 'yes' : 'no'
+})
