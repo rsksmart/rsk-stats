c0dd92b6ba539ffcb90ee869efb52d8f72dafa3b
emilio <emiliorizzo@gmail.com>
2017-09-05 03:14:29 -0300

    Refactor table fields to config

---
+++ src/config/table.js
@@ -0,0 +1,16 @@
+export const fields = [
+  'name',
+  'type',
+  'mining',
+  'latency',
+  'peers',
+  'pending',
+  'lastBlock',
+  'totalDiff',
+  'blockTrans',
+  'uncles',
+  'lastBlockTime',
+  'propTime',
+  'avgPropTime',
+  'uptime'
+]
