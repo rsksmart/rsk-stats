60013a01b0cd01cf176eb5f32d4f3c1342d74a22
emilio <emiliorizzo@gmail.com>
2017-09-25 08:47:11 -0300

    Update app/entity add status

--- src/store/modules/app/entity/actions.js
+++ src/store/modules/app/entity/actions.js
@@ -13,6 +13,7 @@ export const createEntity = ({ state, commit, getters }, name) => {
     entity.thValue = () => { return null }
     entity.colorValue = (value) => { return value }
     entity.color = (value) => { return '' }
+    entity.status = (value) => { return null }
     entity.filteredValue = (value) => {
       return getters.applyFilter(entity.filters, value)
     }
@@ -42,6 +43,28 @@ export const createEntity = ({ state, commit, getters }, name) => {
           return cv
         }
 
+        if (thObj.status) {
+          entity.status = (value, fields) => {
+            if (undefined === value) return ''
+            let thValue = entity.thValue(fields)
+            if (thValue !== null) value = thValue
+            value = entity.thFilter(value)
+            let tState = thObj.status
+            let status
+            if (!Array.isArray(tState)) {
+              for (let v in tState) {
+                v = Number(v)
+                if (value >= v) status = tState[v]
+              }
+            } else {
+              for (let st of tState) {
+                let v = Number(st.value)
+                if (value >= v) status = st.color
+              }
+            }
+            return { status, number: state.status[status] }
+          }
+        }
         if (thObj.colorFunc) {
           entity.color = (value, fields) => {
             if (undefined === value) return ''
