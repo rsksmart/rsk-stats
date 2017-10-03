832c749f1c4b0cdf777d4b442eeabcb6fce42bc7
emilio <emiliorizzo@gmail.com>
2017-10-03 07:57:20 -0300

    Update MinersChart

--- src/components/MinersChart.vue
+++ src/components/MinersChart.vue
@@ -7,13 +7,21 @@
           th blocks
       tbody
         tr(v-for='miner in miners')
-          td {{ (miner.name) ? miner.name : miner.miner | hash-trim  }}
+          td(v-if='miner.name') 
+            span {{miner.name}} &nbsp;
+            tool-tip(:value='miner.miner' trim='4' :options='{trimAt:"end"}') 
+          td(v-else)
+            tool-tip(:value='miner.miner' :trim='8' :options='{trimAt:"center"}')
           td {{miner.blocks}}
 </template>
 <script>
 import { mapGetters } from 'vuex'
+import ToolTip from './ToolTip.vue'
 export default {
   name: 'miners-chart',
+  components: {
+    ToolTip
+  },
   computed: {
     ...mapGetters('backend/', {
       miners: 'getMiners'
