cfe73be027ab8a8398c7970b652da74caf4c9275
emi <emiliorizzo@gmail.com>
2020-01-15 01:12:22 +0100

    Fix icons colors

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,7 +2,7 @@
   .logo-cont(v-once)
     .logo
       icon.iso(name='rsk' scale='1')
-      span &nbsp; rsk network
+      span rsk network
 </template>
 <script>
 import '../icons/rsk'
@@ -16,13 +16,22 @@ export default {
     display flex
     justify-content center
     align-items center
-    padding:  0
+    padding  0
     min-height $min-box-height
-    .logo
 
+    .logo
+      display flex
+      align-items center
       color $color
-      margin:0 .5em
-      white-space:nowrap
+      margin 0 .5em
+      white-space nowrap
       font-size: 1.75em
+      .iso
+        margin 0 .25em 0 0
+        display flex
+      .svg-icon
+        fill @color
+      span
+        display flex
 
 </style>
