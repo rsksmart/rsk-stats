d9be7f1b4d6e8594e4d90f32c949f912989a354e
emi <emiliorizzo@gmail.com>
2020-11-10 06:49:48 -0700

    Disable box, BTC Hash rate mining

--- src/store/modules/app/state.js
+++ src/store/modules/app/state.js
@@ -33,7 +33,7 @@ export default function (types) {
           minimized: false
         },
         hashrateComparedToBtcNetwork: {
-          show: true,
+          show: false,
           minimized: false
         },
         uncles: {
